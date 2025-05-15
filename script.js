68d063eb987f8293f66f79b43ab25c515e8d50b8
Hamza <baitichhamza@gmail.com>
2025-05-15 15:15:23 +0100

    fix dropdown icone bug

--- script.js
+++ script.js
@@ -4,4 +4,10 @@ const iconMenu = document.querySelector(".dropIcon");
 
 dropdown.addEventListener("click", () => {
   navLinks.classList.toggle("active");
+  dropdown.classList.toggle("opened");
+  if (dropdown.classList.contains("opened")) {
+    iconMenu.src = "images/icon-close-menu.svg";
+  } else {
+    iconMenu.src = "images/icon-menu.svg";
+  }
 });
