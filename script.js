478ff2d3b0348df90bd8620af02cbbaf94c65c94
Hamza <baitichhamza@gmail.com>
2025-05-15 14:57:52 +0100

    some css to nav menu

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const dropdown = document.querySelector(".dropdown");
 const navLinks = document.querySelector(".nav-links");
+const iconMenu = document.querySelector(".dropIcon");
 
 dropdown.addEventListener("click", () => {
   navLinks.classList.toggle("active");
