c718c47e30f98c0648d8a5d5bd645f3074555db8
arthur_wsl <baitichhamza@gmail.com>
2025-05-27 21:46:50 +0100

    add show extra features functionality

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const dropdown = document.querySelector(".dropdown");
 const navLinks = document.querySelector(".nav-links");
 const iconMenu = document.querySelector(".dropIcon");
+const featuresBtn = document.querySelector(".featuresBtn");
+const featuresExpand = document.querySelector(".features-expand");
 
 dropdown.addEventListener("click", () => {
   navLinks.classList.toggle("active");
@@ -10,4 +12,9 @@ dropdown.addEventListener("click", () => {
   } else {
     iconMenu.src = "images/icon-menu.svg";
   }
+});
+
+featuresBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  featuresExpand.classList.toggle("show-features");
 });
