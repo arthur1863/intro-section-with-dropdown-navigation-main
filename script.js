dd0ed2738cc35932e4429fcdb531edca1a15f456
arthur_wsl <baitichhamza@gmail.com>
2025-05-29 13:31:02 +0100

    finish mobile layout

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const navLinks = document.querySelector(".nav-links");
 const iconMenu = document.querySelector(".dropIcon");
 const featuresBtn = document.querySelector(".featuresBtn");
 const featuresExpand = document.querySelector(".features-expand");
+const companyBtn = document.querySelector(".companyBtn");
+const companyExpand = document.querySelector(".company-expand");
 
 dropdown.addEventListener("click", () => {
   navLinks.classList.toggle("active");
@@ -17,4 +19,10 @@ dropdown.addEventListener("click", () => {
 featuresBtn.addEventListener("click", (e) => {
   e.preventDefault();
   featuresExpand.classList.toggle("show-features");
+});
+
+companyBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  companyExpand.classList.toggle("show-company");
+  console.log("hallo");
 });
